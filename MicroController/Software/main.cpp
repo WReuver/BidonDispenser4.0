1502d8f8fb0365186e2bde6832dc7bc363b1792f
Robin C. Pel - Laptop <robinpel@hotmail.com>
2018-03-23 14:10:57 +0100

    Tested the previous changes, they worked fine.

--- MicroController/Software/main.cpp
+++ MicroController/Software/main.cpp
@@ -15,20 +15,28 @@
 
 using namespace Hardware;
 
+static void initialize(void) 
+{
+	SystemClock::SetClockSource(SystemClock::Source::RC32MHz);
+}
+
 int main() 
 {
-    //OSC.CTRL	= 0b00000011;				    // Enable the internal 32 and 2 MHz oscillators
-    //while (! (OSC.STATUS & 0b00000010) );	    // Wait for the 32MHz internal oscillator to be enabled
-    //CCP			= 0xD8;
-    //CLK.CTRL	= 0b00000001;
-    
-    SystemClock::SetClockSource(SystemClock::Source::RC32MHz);              // TODO: Test Whether this works or not
+    initialize();
 	
 	Gpio::SetPinDirection(Gpio::Pin::A0, Hardware::Gpio::Dir::Output);
+	Gpio::SetPinDirection(Gpio::Pin::C0, Hardware::Gpio::Dir::Output);
+	
+	TCC0.CTRLA = 0b00000110;    // Use the regular clock as the source clock and divide it by 2
+	TCC0.CTRLB = 0b11110011;    // Single Slope PWM mode
+	//TCC0.CTRLC = 
+	//TCC0.CTRLD = 
+	//TCC0.CTRLE = 
 	
 	while (1) 
 	{
 		Gpio::TogglePinValue(Gpio::Pin::A0);
 		_delay_ms(250);
 	}
+	
 }
