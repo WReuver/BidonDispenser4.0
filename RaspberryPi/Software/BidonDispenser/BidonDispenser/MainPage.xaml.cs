a838d82a8cf6e7a8c2c07112b93a6808e9b337ea
Robin C. Pel - Laptop <robinpel@hotmail.com>
2018-04-03 09:24:28 +0200

    Change of plans, instead of Raspbian we wil be running Windows 10 IoT Core on the Raspberry Pi. This means an UWP app will have to be written instead of a javafx app.
    Changes:
    Added the base project for the UWP app.

---
+++ RaspberryPi/Software/BidonDispenser/BidonDispenser/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace BidonDispenser
+{
+    public sealed partial class MainPage : Page
+    {
+        public sealed partial class MainPage : Page {
+            private HelloViewModel viewModel = new HelloViewModel();
+
+            public MainPage() {
+                this.InitializeComponent();
+                DispatcherTimer t = new DispatcherTimer();
+                t.Interval = TimeSpan.FromSeconds(0.5);
+                t.Tick += T_Tick;
+                t.Start();
+            }
+
+            static int index = 0;
+            private void T_Tick(object sender, object e) {
+                index++;
+                viewModel.DisplayLanguage = (HelloViewModel.Language)(index % 3);
+            }
+        }
+    }
+}
